158ad9680c13f527ead3681adb27e9ed9036d55e
spg <SPGGOGOGO@outlook.com>
2024-03-21 11:20:17 +0800

    增加doc文件夹

---
+++ doc/readme.md
@@ -0,0 +1,3 @@
+aCoral文档请看[github wiki](https://github.com/spg-one/aCoral-riscv-cl/wiki)
+
+doc/pic文件夹里面是wiki用的图片
--- src/acoral/src/hal/hal_timer.c
+++ src/acoral/src/hal/hal_timer.c
@@ -7,7 +7,7 @@ int hal_timer_init(int ticks_per_sec){
 #include "clint.h"
 extern void acoral_ticks_entry(int vector);
 	clint_timer_init();                           	/*这个主要用于将用于ticks的时钟初始化*/
-	clint_timer_register(acoral_ticks_entry,NULL);	/*这个用于注册ticks的处理函数*/
+	clint_timer_register(acoral_ticks_entry,NULL);	//SPG 这里不应该直接使用acoral_ticks_entry，因为这是kernel层函数，应该将其作为参数传进来
     return clint_timer_start(1000/ticks_per_sec,0);
 #endif
 
